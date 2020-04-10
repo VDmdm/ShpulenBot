d5b0621052f4dc607b1ce228193f7b029993218f
VDmdm <dmitrytob@gmail.com>
2020-04-10 21:50:23 +0500

    structure

--- подготовка/file_rename.py
+++
@@ -1,8 +0,0 @@
-import os
-
-nbr = 0
-for dirpath, dirnames, filenames in os.walk('D:\\Users\\dmitr\\Desktop\\GitHub\\ShpulenBot'):
-    for filename in filenames:
-        print(filename)
-        os.rename(filename, f'./photo/{nbr}.jpg')
-        nbr += 1
